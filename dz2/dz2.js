75b4784d8e439db87cdcdcf80e823d9f08e1cd7d
olya <lol79lol97@gmail.com>
2021-02-19 11:30:22 +0200

    hw 2 after peer-rewiew

--- dz2/dz2.js
+++ dz2/dz2.js
@@ -1,17 +1,23 @@
+//1. Отримайте у користувача число(N) від якого ми будемо складати. 
+//Отримати число можна за допомогою prompt. Пам'ятайте, ЧИСЛО, не РЯДОК!
+//2.Перевірте, чи є передане значення N ЦІЛИМ числом. 
+//(Підказка: якщо при конвертації в ціле число ми отримали NaN – це число нам не підходить)
 let userNNumber;
 do {
-    userNNumber = parseInt(prompt('Put down your number'));
+    userNNumber = parseInt(prompt('Put down first(smaller than second) N-number'));
 } while(Number.isNaN(userNNumber));
-
 console.log(`You wrote down the N-number ${userNNumber}`);
 
+//3.Отримайте у користувача число(M) до якого ми будемо складати (включно).
+//4.Перевірте, чи є передане значення M ЦІЛИМ числом. 
 let userMNumber;
 do {
-    userMNumber = parseInt(prompt('Put down your number'));
+    userMNumber = parseInt(prompt('Put down second(bigger than first) M-number'));
 } while(Number.isNaN(userMNumber));
+console.log(`You wrote down the M-number. It is ${userMNumber}`);
 
-console.log(`You wrote down the M-number ${userMNumber}`);
-
+//5.Отримайте у користувача булевий параметр(true/false), який підкаже нам чи потрібно 
+//пропускати парні числа. TRUE – потрібно, FALSE – не потрібно. Використовуйте функцію confirm.
 function missEvenNumber() {
     if(confirm("Do you want to miss all even numbers? Are you sure?")){
         return true;
@@ -21,6 +27,9 @@ function missEvenNumber() {
 }
 const trueOrFalse = missEvenNumber();
 
+//6.Напишіть цикл, який буде складати числа від раніше отриманих N и M
+//7.В циклі додайте перевірку – чи потрібно пропускати при складанні парні числа. 
+//9.Виведіть результат
 let factorialFromNToM = 0;
 for(let count = userNNumber; count <= userMNumber; count++){
     if(trueOrFalse === true){
