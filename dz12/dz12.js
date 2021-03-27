a3dad73cff7b7237511ddc21c7b1adbae2b853c4
olya <lol79lol97@gmail.com>
2021-03-27 23:42:50 +0200

    12dz

--- dz12/dz12.js
+++ dz12/dz12.js
@@ -1,38 +1,15 @@
-// function getRandomChineses(length = 4){
-//     let letter;
-//     for(let i = 0; i < length; i++){
-//         let date = Date.now()
-//         let sign = (date.toString()).substring(8, 13)
-//         letter = String.fromCharCode(sign)
-//     }
-//     return letter;
-// }
-
-// getRandomChinese()
-//console.log(getRandomChinese())
-
-
-//А далі вантажівка з костилями підїхала
-
-function getRandomChinese(length = 4) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        let letter;
-        for(let i = 0; i < length; i++){
+async function getRandomChinese(length = 4){
+    let letters = "";
+        for (let i = 0; i < length; i++) { 
             let date = Date.now()
-            let sign = (date.toString()).substring(8, 13)
-            letter = String.fromCharCode(sign)
-            console.log(letter)
-            resolve(letter)
+            let sign = (date.toString()).substring(-5)
+            let letter = String.fromCharCode(sign)
+            letters = letters + ' ' + letter;
+            await new Promise((resolve) => {
+                setTimeout(() => {resolve(letter)}, 50)
+            })  
         }
-      },length * 50)
-    })
-    
-  }
-
-let string = ""
-
-getRandomChinese(1)
-.then(() => getRandomChinese(1))
-.then(() => getRandomChinese(1))
-.then(() => getRandomChinese(1))
+    console.log(letters);
+    return letters;    
+}
+getRandomChinese(8);
